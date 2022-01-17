9f5dd743536224e67c57656c3e4946189994026b
cryptopandamc <junetefl@gmail.com>
2022-01-17 11:13:32 +0000

    adds a creat question method to api, tested in talend

--- src/main/java/com/june/revisiontool/api/RestApi.java
+++ src/main/java/com/june/revisiontool/api/RestApi.java
@@ -3,13 +3,19 @@ package com.june.revisiontool.api;
 import java.util.List;
 import java.util.Optional;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.june.revisiontool.model.Question;
 import com.june.revisiontool.service.QuestionService;
@@ -35,5 +41,13 @@ public class RestApi {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(question.get());
+	}
+	
+	@PostMapping("CreateQuestion")
+	public ResponseEntity<Void> createQuestion(@RequestBody Question question, BindingResult bindingResult)  {
+		questionService.create(question);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{questionId}").buildAndExpand(question.getQuestionId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
