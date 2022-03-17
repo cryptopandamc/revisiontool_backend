a82276c4ea705e9b304b2f1c2fea865ccb7ceb91
cryptopandamc <junetefl@gmail.com>
2022-03-17 15:29:46 +0000

    added a search by tag test returns all questions with that tag

--- src/main/java/com/june/revisiontool/repository/QuestionDao.java
+++ src/main/java/com/june/revisiontool/repository/QuestionDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.june.revisiontool.model.Answer;
 import com.june.revisiontool.model.Question;
 import com.june.revisiontool.model.Tag;
 
